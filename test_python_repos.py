fd819f524ae7c5d2cf8027156beef70982d1f15d
Jacek Kikowski <jacek.kikowski@wp.pl>
2023-08-02 17:20:30 +0200

    Add files via upload

---
+++ test_python_repos.py
@@ -0,0 +1,15 @@
+import unittest
+from python_repos import r
+
+#asercja czy status_code == 200
+
+class Status_codeTestCase(unittest.TestCase):
+	"""Test dla programu python_repos.py"""
+
+	def test_status_code(self):
+		"""Czy status_code jest równy 200"""
+		status = r.status_code
+		self.assertEqual(status,200)
+
+if __name__ == '__main__':
+	unittest.main()
